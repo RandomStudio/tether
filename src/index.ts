d88ee15a65a87f02ce970be2ca6f06f258f81f48
Stephen Buchanan <stephen.buchanan@random.studio>
2021-05-12 09:35:48 +0200

    POC basic encode and decode

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
+import MsgPack from "msgpack5";
+
 import defaults from "./defaults";
 import parse from "parse-strings-in-object";
 import rc from "rc";
 import { getLogger } from "log4js";
 
-const config: typeof defaults = parse(rc("lights", defaults));
+const config: typeof defaults = parse(rc("tether2", defaults));
 
 const logger = getLogger();
 logger.level = config.loglevel;
 
 logger.info("started with config", config);
+
+const msgpack = MsgPack();
+
+const encoded = msgpack.encode({ hello: "world" });
+
+console.log({ number: encoded, str: encoded.toString("hex") });
+// 81a568656c6c6fa5776f726c64s
+
+const incoming = Buffer.from([
+  0x81,
+  0xa5,
+  0x68,
+  0x65,
+  0x6c,
+  0x6c,
+  0x6f,
+  0xa5,
+  0x77,
+  0x6f,
+  0x72,
+  0x6c,
+  0x64,
+]);
+
+console.log({
+  actual: msgpack.decode(encoded),
+  shouldMatch: msgpack.decode(incoming),
+});
