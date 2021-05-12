45c08db4e409fa3c90161ac5f15b09346cf67de4
Stephen Buchanan <stephen.buchanan@random.studio>
2021-05-12 10:04:28 +0200

    basic explorer component sends to itself

--- explorer/src/Connection/Connection.tsx
+++ explorer/src/Connection/Connection.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import mqtt, { MqttClient } from "mqtt";
 
 interface ConnectionProps {
   path: string;
@@ -12,6 +13,8 @@ interface ConnectionState {
   received: number;
 }
 
+let client: MqttClient | null = null;
+
 class Connection extends React.Component<ConnectionProps, ConnectionState> {
   constructor(props: ConnectionProps) {
     super(props);
@@ -20,6 +23,29 @@ class Connection extends React.Component<ConnectionProps, ConnectionState> {
       sent: 0,
       received: 0,
     };
+  }
+
+  componentDidMount() {
+    const { address, port, path } = this.props;
+    client = mqtt.connect(`${address}:${port}${path}`);
+
+    client.on("connect", () => {
+      console.log("connected!");
+      this.setState({ connected: true });
+      client?.subscribe("presence", (err) => {
+        if (!err) {
+          client?.publish("presence", "Hello mqtt");
+          this.setState({ sent: this.state.sent + 1 });
+        }
+      });
+    });
+
+    client.on("message", (topic, message) => {
+      // message is Buffer
+      console.log(message.toString());
+      this.setState({ received: this.state.received + 1 });
+      // client?.end()
+    });
   }
 
   render() {
