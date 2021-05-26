6676df6f0434853a8e0409fa990765bbebac68b6
Stephen Buchanan <stephen.buchanan@random.studio>
2021-05-26 16:35:48 +0200

    updated cli example with new msgpack library

--- examples/cli/tether-receive.js
+++ examples/cli/tether-receive.js
@@ -1,17 +1,16 @@
 const mqtt = require("async-mqtt");
-const MsgPack = require("msgpack5");
 const rc = require("rc");
 const parse = require("parse-strings-in-object");
-const msgpack5 = require("msgpack5");
+const { decode } = require("@msgpack/msgpack");
 
-const config = rc("tetherSend", {
-  protocol: "tcp",
-  host: "localhost",
-  port: 1883,
-  topic: "#",
-});
-
-const msgPack = new MsgPack();
+const config = parse(
+  rc("tetherSend", {
+    protocol: "tcp",
+    host: "localhost",
+    port: 1883,
+    topic: "#",
+  })
+);
 
 const run = async () => {
   const { protocol, host, port } = config;
@@ -28,7 +27,7 @@ const run = async () => {
 
   client.subscribe(topic);
   client.on("message", (topic, message) => {
-    const decoded = msgPack.decode(message);
+    const decoded = decode(message);
     console.log(
       `received message on topic "${topic}": ${JSON.stringify(decoded)}`
     );
