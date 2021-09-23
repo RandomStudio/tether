27603d3711e1c6b5a250f88f9a3e035b6a29af25
Stephen Buchanan <stephen.buchanan@random.studio>
2021-09-23 10:38:51 +0200

    basic untimed bulk send from JSON array

--- utilities/cli/tether-send.js
+++ utilities/cli/tether-send.js
@@ -3,6 +3,8 @@ const rc = require("rc");
 const parse = require("parse-strings-in-object");
 const { encode } = require("@msgpack/msgpack");
 const { getLogger } = require("log4js");
+const path = require("path");
+const fs = require("fs/promises");
 
 const config = parse(
   rc("tetherSend", {
@@ -15,6 +17,10 @@ const config = parse(
     username: "tether",
     password: "sp_ceB0ss!",
     path: "",
+    jsonReader: {
+      enabled: false,
+      path: "./data.json",
+    },
   })
 );
 
@@ -38,17 +44,38 @@ const sendMessages = (client, message, topic) => {
   }
 };
 
-const main = async () => {
-  const { protocol, host, port, username, password, path } = config;
+const sendFromJson = async (client, filePath) => {
+  const res = await fs.readFile(filePath);
+  logger.trace({ res });
+  const json = JSON.parse(res);
+  logger.trace({ json });
+  logger.info("Read array from JSON of length", json.length);
+  json.forEach((entry) => {
+    const { topic } = entry;
+    const encoded = encode(entry);
+    client.publish(topic, Buffer.from(encoded));
+  });
+};
 
-  const url = `${protocol}://${host}:${port}${path}`;
+const main = async () => {
+  const { protocol, host, port, username, password } = config;
+
+  const url = `${protocol}://${host}:${port}${config.path}`;
 
   logger.info("Connecting to MQTT broker @", url, "...");
 
   try {
     const client = await mqtt.connectAsync(url, { username, password });
     logger.info("...connected OK");
-    sendMessages(client, config.message, config.topic);
+
+    if (config.jsonReader) {
+      const filePath = path.resolve(config.jsonReader.path);
+      logger.info("jsonReader enabled; will read from file", filePath, "...");
+      await sendFromJson(client, filePath);
+    } else {
+      logger.debug("jsonReader not enabled; will send single message");
+      sendMessages(client, config.message, config.topic);
+    }
 
     // TODO: should loop / REPL before closing
     client.end();
