472b4b72795719a25efa77b04f08e98b763425a4
Stephen Buchanan <stephen.buchanan@random.studio>
2021-09-23 15:11:16 +0200

    dummy chart data inserted

--- utilities/tether-graphs/src/App.svelte
+++ utilities/tether-graphs/src/App.svelte
@@ -1,15 +1,46 @@
 <script>
-  export let files;
+  import { Chart, registerables } from "chart.js";
+  Chart.register(...registerables);
+
+  let files;
+  let canvas;
 
   $: if (files) {
     console.log("file", files);
   }
 
-  function drawGraph(file) {
+  function drawGraph(jsonString) {
+    const json = JSON.parse(jsonString);
+
+    const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+        },
+      ],
+    };
+
+    const config = {
+      type: "line",
+      data: data,
+    };
+
+    const ctx = canvas.getContext("2d");
+    const c = new Chart(ctx, config);
+  }
+
+  function uploadFile(file) {
     console.log("read file", file);
     const reader = new FileReader();
     reader.onload = (e) => {
       console.log("data:", e.target.result);
+      drawGraph(e.target.result);
     };
     reader.readAsText(file);
   }
@@ -29,9 +60,13 @@
     <h2>Selected files:</h2>
     {#each Array.from(files) as file}
       <p>{file.name} ({file.size} bytes)</p>
-      <button on:click={drawGraph(file)}>Draw Graph</button>
+      <button on:click={uploadFile(file)}>Draw Graph</button>
     {/each}
   {/if}
+
+  <div>
+    <canvas bind:this={canvas} width={400} height={400} />
+  </div>
 </main>
 
 <style>
