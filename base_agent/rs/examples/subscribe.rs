43478aca4411286ba11964d1ef575597b4c186a2
Stephen Buchanan <stephen@pop-os.localdomain>
2023-06-27 11:22:05 +0200

    subscribe example includes decoding example

--- base_agent/rs/examples/subscribe.rs
+++ base_agent/rs/examples/subscribe.rs
@@ -1,8 +1,18 @@
 use std::{thread, time::Duration};
 
 use env_logger::{Builder, Env};
-use log::{debug, info};
+use log::{debug, info, warn};
+use rmp_serde::from_slice;
+use serde::Deserialize;
 use tether_agent::TetherAgent;
+
+#[derive(Deserialize, Debug)]
+struct CustomMessage {
+    id: usize,
+    name: String,
+}
+// Test this by sending a message like
+// tether-send --host localhost --topic test/any/two --message \{\"id\":1,\"name\":\"boo\"\}
 
 fn main() {
     println!("Rust Tether Agent subscribe example");
@@ -26,7 +36,7 @@ fn main() {
         info!("#{i}: Checking for messages...");
         if let Some((plug_name, message)) = agent.check_messages() {
             if &input_one.name == plug_name.as_str() {
-                println!(
+                info!(
                     "******** INPUT ONE:\n Received a message from plug named \"{}\" on topic {} with length {} bytes",
                     input_one.name,
                     message.topic(),
@@ -34,15 +44,21 @@ fn main() {
                 );
             }
             if &input_two.name == plug_name.as_str() {
-                println!(
+                info!(
                     "******** INPUT TWO:\n Received a message from plug named \"{}\" on topic {} with length {} bytes",
                     input_two.name,
                     message.topic(),
                     message.payload().len()
                 );
+                // Notice how you must give the from_slice function a type so it knows what to expect
+                let decoded = from_slice::<CustomMessage>(&message.payload());
+                match decoded {
+                    Ok(d) => { info!("Yes, we decoded the MessagePack payload as: {:?}", d)},
+                    Err(e) => { warn!("Failed to decode the payload: {}", e)}
+                };
             }
             if &input_empty.name == plug_name.as_str() {
-                println!(
+                info!(
                     "******** EMPTY MESSAGE:\n Received a message from plug named \"{}\" on topic {} with length {} bytes",
                     input_empty.name,
                     message.topic(),
