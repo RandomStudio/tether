7cf96e49e7a156b9e1eedd18531d8e160094ce8b
stephen <stephen.buchanan@random.studio>
2023-06-30 16:08:38 +0200

    decent overrides for tether-send

--- utilities/tether-cli/src/bin/tether-send.rs
+++ utilities/tether-cli/src/bin/tether-send.rs
@@ -1,6 +1,6 @@
-use cli_shared::defaults::AGENT_ROLE;
+use cli_shared::defaults::{AGENT_ID, AGENT_ROLE};
 use env_logger::{Builder, Env};
-use log::debug;
+use log::{debug, info, warn};
 use serde::Serialize;
 use tether_agent::{PlugOptionsBuilder, TetherAgentOptionsBuilder};
 
@@ -19,17 +19,21 @@ use clap::{command, Parser};
 pub struct Cli {
     /// Specify an Agent Role; this will be used for the auto-generated publish topic
     /// (ignored if you provide your own plug.topic)
-    #[arg(long = "agent.role")]
-    pub agent_role: Option<String>,
+    #[arg(long = "agent.role", default_value_t=String::from(AGENT_ROLE))]
+    pub agent_role: String,
 
     /// Specify an Agent ID or Group; this will be used for the auto-generated publish topic
     /// (ignored if you provide your own plug.topic)
-    #[arg(long = "agend.id")]
-    pub agent_id: Option<String>,
+    #[arg(long = "agent.id", default_value_t=String::from(AGENT_ID))]
+    pub agent_id: String,
 
     /// Overide the auto-generated topic with your own, to use with every published message
-    #[arg(long = "plug.topic", default_value_t=String::from(format!("{}/any/testMessages", AGENT_ROLE)))]
-    pub plug_topic: String,
+    #[arg(long = "plug.name", default_value_t=String::from("testMessages"))]
+    pub plug_name: String,
+
+    /// Overide the auto-generated topic with your own, to use with every published message
+    #[arg(long = "plug.topic")]
+    pub plug_topic: Option<String>,
 
     #[arg(long = "loglevel",default_value_t=String::from("info"))]
     pub log_level: String,
@@ -43,18 +47,30 @@ fn main() {
 
     debug!("Debugging is enabled; could be verbose");
 
-    let agent_role = match cli.agent_role {
-        Some(custom) => custom,
-        None => AGENT_ROLE.into(),
+    let publish_topic = match cli.plug_topic {
+        Some(override_topic) => {
+            warn!(
+                "Using override topic \"{}\"; agent role, agent ID and plug name will be ignored",
+                override_topic
+            );
+            override_topic
+        }
+        None => {
+            let auto_generated_topic =
+                format!("{}/{}/{}", &cli.agent_role, &cli.agent_id, &cli.plug_name);
+            info!("Using auto-generated topic \"{}\"", &auto_generated_topic);
+            auto_generated_topic
+        }
     };
 
-    debug!("Using agent role = {}", &agent_role);
-
-    let tether_agent = TetherAgentOptionsBuilder::new(&agent_role)
+    let tether_agent = TetherAgentOptionsBuilder::new(&cli.agent_role)
+        .id(&cli.agent_id)
         .build()
         .expect("failed to connect Tether");
 
-    let output = PlugOptionsBuilder::create_output("dummy").build(&tether_agent);
+    let output = PlugOptionsBuilder::create_output(&cli.plug_name)
+        .topic(&publish_topic)
+        .build(&tether_agent);
 
     let dummy_data = DummyData {
         id: 0,
--- utilities/tether-cli/src/lib.rs
+++ utilities/tether-cli/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod defaults {
     pub const AGENT_ROLE: &str = "testAgent";
+    pub const AGENT_ID: &str = "any";
 }
