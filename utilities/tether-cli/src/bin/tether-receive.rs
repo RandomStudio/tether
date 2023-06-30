3e8402f7769c827b6d4f3fcebb703f2146c20362
stephen <stephen.buchanan@random.studio>
2023-06-30 15:44:02 +0200

    cli args for tether-receive

--- utilities/tether-cli/src/bin/tether-receive.rs
+++ utilities/tether-cli/src/bin/tether-receive.rs
@@ -1,24 +1,40 @@
+use cli_shared::defaults::AGENT_ROLE;
 use env_logger::{Builder, Env};
 use log::{debug, info};
 use tether_agent::{PlugOptionsBuilder, TetherAgentOptionsBuilder};
 
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[arg(long = "topic", default_value_t=String::from("#"))]
+    pub subscribe_topic: String,
+
+    #[arg(long = "loglevel",default_value_t=String::from("info"))]
+    pub log_level: String,
+}
+
 fn main() -> ! {
-    let mut builder = Builder::from_env(Env::default().default_filter_or("info"));
+    let cli = Cli::parse();
+
+    let mut builder = Builder::from_env(Env::default().default_filter_or(cli.log_level));
     builder.init();
 
     debug!("Debugging is enabled; could be verbose");
 
-    let tether_agent = TetherAgentOptionsBuilder::new("testAgent")
+    let tether_agent = TetherAgentOptionsBuilder::new(AGENT_ROLE)
         .build()
         .expect("failed to connect Tether");
 
     let _input = PlugOptionsBuilder::create_input("all")
-        .topic("#")
+        .topic(&cli.subscribe_topic)
         .build(&tether_agent);
 
     loop {
         while let Some((plug_name, message)) = tether_agent.check_messages() {
             debug!("Received message on plug {}: {:?}", plug_name, message);
+            info!("Received message on topic \"{}\"", message.topic());
             let bytes = message.payload();
             if bytes.is_empty() {
                 info!("Empty message payload");
---
+++ utilities/tether-cli/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod defaults {
+    pub const AGENT_ROLE: &str = "testAgent";
+}
