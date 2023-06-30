58eba0ff8ed5d2a449e5bfd191706113e3b5a7ea
stephen <stephen.buchanan@random.studio>
2023-06-30 15:57:09 +0200

    basic overrides for tether-send

--- utilities/tether-cli/src/bin/tether-send.rs
+++ utilities/tether-cli/src/bin/tether-send.rs
@@ -1,3 +1,6 @@
+use cli_shared::defaults::AGENT_ROLE;
+use env_logger::{Builder, Env};
+use log::debug;
 use serde::Serialize;
 use tether_agent::{PlugOptionsBuilder, TetherAgentOptionsBuilder};
 
@@ -9,8 +12,45 @@ struct DummyData {
     a_string: String,
 }
 
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    /// Specify an Agent Role; this will be used for the auto-generated publish topic
+    /// (ignored if you provide your own plug.topic)
+    #[arg(long = "agent.role")]
+    pub agent_role: Option<String>,
+
+    /// Specify an Agent ID or Group; this will be used for the auto-generated publish topic
+    /// (ignored if you provide your own plug.topic)
+    #[arg(long = "agend.id")]
+    pub agent_id: Option<String>,
+
+    /// Overide the auto-generated topic with your own, to use with every published message
+    #[arg(long = "plug.topic", default_value_t=String::from(format!("{}/any/testMessages", AGENT_ROLE)))]
+    pub plug_topic: String,
+
+    #[arg(long = "loglevel",default_value_t=String::from("info"))]
+    pub log_level: String,
+}
+
 fn main() {
-    let tether_agent = TetherAgentOptionsBuilder::new("testAgent")
+    let cli = Cli::parse();
+
+    let mut builder = Builder::from_env(Env::default().default_filter_or(cli.log_level));
+    builder.init();
+
+    debug!("Debugging is enabled; could be verbose");
+
+    let agent_role = match cli.agent_role {
+        Some(custom) => custom,
+        None => AGENT_ROLE.into(),
+    };
+
+    debug!("Using agent role = {}", &agent_role);
+
+    let tether_agent = TetherAgentOptionsBuilder::new(&agent_role)
         .build()
         .expect("failed to connect Tether");
 
