4a308548be3d561ad7d2b5b3c0b0f76eabf65d17
stephen <stephen.buchanan@random.studio>
2023-07-03 17:16:00 +0200

    override topic works

--- utilities/tether-cli/src/tether_playback.rs
+++ utilities/tether-cli/src/tether_playback.rs
@@ -1,14 +1,11 @@
 use std::{fs::File, io::BufReader};
 
 use clap::Args;
-use log::{debug, error, info, warn};
-use rmp_serde::from_slice;
+use log::{debug, info, warn};
+
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use tether_agent::{
-    OutputPlugDefinition, PlugDefinition, PlugDefinitionCommon, PlugOptionsBuilder, TetherAgent,
-    TetherAgentOptionsBuilder,
-};
+use tether_agent::{TetherAgent, TetherAgentOptionsBuilder};
 
 use crate::{defaults, Cli};
 
@@ -34,16 +31,24 @@ pub struct PlaybackOptions {
     file_path: String,
 
     /// Overide any original topics saved in the file, to use with every published message
-    #[arg(long = "overrideTopic")]
+    #[arg(long = "topic")]
     override_topic: Option<String>,
 
-    /// Flag to enable infinite looping for playback
+    /// How many times to loop playback
+    #[arg(long = "loops.count", default_value_t = 1)]
+    loop_count: usize,
+
+    /// Flag to enable infinite looping for playback (ignore loops.count if enabled)
     #[arg(long = "loops.infinite")]
     loop_infinite: bool,
 }
 
 pub fn playback(cli: &Cli, options: &PlaybackOptions) {
     info!("Tether Playback Utility");
+
+    if let Some(t) = &options.override_topic {
+        warn!("Override topic provided; ALL topics in JSON entries will be ignored and replaced with \"{}\"",t);
+    }
 
     let tether_agent = TetherAgentOptionsBuilder::new(defaults::AGENT_ROLE)
         .host(&cli.tether_host)
@@ -53,21 +58,26 @@ pub fn playback(cli: &Cli, options: &PlaybackOptions) {
         .build()
         .expect("failed to connect Tether");
 
-    // let output = PlugOptionsBuilder::create_output(&options.plug_name)
-    //     .topic(&publish_topic)
-    //     .build(&tether_agent);
-
     if options.loop_infinite {
-        info!("Infinite loops requested; Press Ctr+C to stop");
         loop {
-            parse_json_rows(&options.file_path, &tether_agent);
+            warn!("Infinite loops requested; Press Ctr+C to stop");
+            parse_json_rows(&options.file_path, &tether_agent, &options.override_topic);
         }
     } else {
-        parse_json_rows(&options.file_path, &tether_agent);
+        let mut count = 0;
+        while count < options.loop_count {
+            count += 1;
+            info!(
+                "Finite loops requested: starting loop {}/{}",
+                count, options.loop_count
+            );
+            parse_json_rows(&options.file_path, &tether_agent, &options.override_topic);
+        }
+        info!("All {} loops completed", options.loop_count);
     }
 }
 
-fn parse_json_rows(filename: &str, tether_agent: &TetherAgent) {
+fn parse_json_rows(filename: &str, tether_agent: &TetherAgent, override_topic: &Option<String>) {
     let file = File::open(filename).expect(&format!("failed to open file {}", filename));
     let reader = BufReader::new(file);
     let deserializer = serde_json::Deserializer::from_reader(reader);
@@ -88,6 +98,11 @@ fn parse_json_rows(filename: &str, tether_agent: &TetherAgent) {
 
         debug!("Sleeping {}ms ...", delta_time);
         std::thread::sleep(std::time::Duration::from_millis(*delta_time));
+
+        let topic = match &override_topic {
+            Some(t) => String::from(t),
+            None => String::from(topic),
+        };
 
         tether_agent
             .publish_raw(&topic, payload, None, None)
