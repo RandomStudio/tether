44c16daa391265a0ffdd55d47dcc89239ad1cc99
stephen <stephen.buchanan@random.studio>
2023-07-03 11:17:47 +0200

    topics subcommand works

--- utilities/tether-cli/src/main.rs
+++ utilities/tether-cli/src/main.rs
@@ -1,7 +1,10 @@
 use env_logger::{Builder, Env};
 use log::{debug, error, info, warn};
 use serde::Serialize;
-use tether_agent::{PlugOptionsBuilder, TetherAgentOptionsBuilder};
+use tether_agent::{
+    mqtt::Message, parse_agent_id, parse_agent_role, parse_plug_name, PlugOptionsBuilder,
+    TetherAgentOptionsBuilder,
+};
 
 use clap::{Args, Parser, Subcommand};
 
@@ -77,6 +80,52 @@ enum Commands {
     Receive(ReceiveOptions),
     Send(SendOptions),
     Topics(TopicOptions),
+}
+
+#[derive(Debug)]
+pub struct Insights {
+    topics: Vec<String>,
+    roles: Vec<String>,
+    ids: Vec<String>,
+    plugs: Vec<String>,
+    // message_count: u64,
+}
+
+impl Insights {
+    fn new() -> Self {
+        Insights {
+            topics: Vec::new(),
+            roles: Vec::new(),
+            ids: Vec::new(),
+            plugs: Vec::new(),
+            // message_count: 0,
+        }
+    }
+
+    pub fn update(&mut self, _plug_name: &str, message: &Message) {
+        // self.message_count += 1;
+
+        // Collect some stats...
+        add_if_unique(message.topic(), &mut self.topics);
+        add_if_unique(
+            parse_agent_role(message.topic()).unwrap_or("unknown"),
+            &mut self.roles,
+        );
+        add_if_unique(
+            parse_agent_id(message.topic()).unwrap_or("unknown"),
+            &mut self.ids,
+        );
+        add_if_unique(
+            parse_plug_name(message.topic()).unwrap_or("unknown"),
+            &mut self.plugs,
+        );
+    }
+}
+
+fn add_if_unique(item: &str, list: &mut Vec<String>) {
+    if !list.iter().any(|i| i.eq(item)) {
+        list.push(String::from(item));
+    }
 }
 
 mod defaults {
@@ -180,6 +229,27 @@ fn send(cli: &Cli, options: &SendOptions) {
 
 fn topics(cli: &Cli, options: &TopicOptions) {
     info!("Tether Topics Parsing Utility");
+
+    let tether_agent = TetherAgentOptionsBuilder::new(defaults::AGENT_ROLE)
+        .host(&cli.tether_host)
+        .port(cli.tether_port)
+        .username(&cli.tether_username)
+        .password(&cli.tether_password)
+        .build()
+        .expect("failed to connect Tether");
+
+    let _input = PlugOptionsBuilder::create_input("all")
+        .topic(&options.subscribe_topic)
+        .build(&tether_agent);
+
+    let mut insights = Insights::new();
+
+    loop {
+        while let Some((plug_name, message)) = tether_agent.check_messages() {
+            insights.update(&plug_name, &message);
+            info!("{:#?}", insights);
+        }
+    }
 }
 
 fn main() {
