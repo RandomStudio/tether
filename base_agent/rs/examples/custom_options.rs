d62440a265c2ba9a6a4522e5aae66eb3684c00d7
stephen <stephen.buchanan@random.studio>
2023-10-05 10:01:01 +0200

    some options are optional

--- base_agent/rs/examples/custom_options.rs
+++ base_agent/rs/examples/custom_options.rs
@@ -2,7 +2,7 @@ use tether_agent::{PlugOptionsBuilder, TetherAgentOptionsBuilder};
 
 fn main() {
     let tether_agent = TetherAgentOptionsBuilder::new("example")
-        .id("customId")
+        .id_optional(None)
         .host("localhost")
         .port(1883)
         .username("tether")
@@ -10,13 +10,19 @@ fn main() {
         .build()
         .expect("failed to create Tether Agent");
 
-    let _output_plug = PlugOptionsBuilder::create_output("anOutput")
+    let output_plug = PlugOptionsBuilder::create_output("anOutput")
         .qos(2)
         .retain(true)
         .build(&tether_agent);
-    let _input_plug = PlugOptionsBuilder::create_input("everything")
+    let input_plug = PlugOptionsBuilder::create_input("everything")
         .topic("#")
         .build(&tether_agent);
 
-    // And then proceed as usual!
+    println!("Agent looks like this: {:?}", tether_agent.description());
+    let (role, id, _) = tether_agent.description();
+    assert_eq!(role, "example");
+    assert_eq!(id, "any"); // because we set None
+
+    println!("output plug: {:?}", output_plug);
+    println!("input plug: {:?}", input_plug);
 }
