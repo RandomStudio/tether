5e5791c371eb1ede9b462c4c589a839604fb3f8c
Stephen Buchanan <webtvmaster@gmail.com>
2025-04-08 15:51:44 +0200

    React example updated

--- examples/react-ts/src/Tether/Receiver.tsx
+++ examples/react-ts/src/Tether/Receiver.tsx
@@ -1,35 +1,37 @@
 import { useEffect, useState } from "react";
-import { InputPlug, TetherAgent } from "tether-agent";
+import { ChannelReceiver, TetherAgent } from "tether-agent";
 
 interface Props {
   agent: TetherAgent;
 }
 
 export const Receiver = (props: Props) => {
-  const [plug, setPlug] = useState<InputPlug | null>(null);
+  const [channel, setChannel] = useState<ChannelReceiver | null>(null);
   const [lastMessage, setLastMessage] = useState("");
 
   useEffect(() => {
-    InputPlug.create(props.agent, "everything", {
+    ChannelReceiver.create(props.agent, "everything", {
       overrideTopic: "#",
     })
-      .then((plug) => {
-        setPlug(plug);
-        plug.on("message", (payload, topic) => {
+      .then((channel) => {
+        setChannel(channel);
+        channel.on("message", (payload, topic) => {
           console.log("Received message on", topic);
           const timestamp = Date.now();
           setLastMessage(JSON.stringify({ topic, payload, timestamp }));
         });
       })
       .catch((e) => {
-        console.error("Error creating InputPlug:", e);
+        console.error("Error creating Channel Receiver:", e);
       });
   }, [props.agent]);
 
   return (
     <div>
-      <h2>Input Plug</h2>
-      <div>{plug && <code>{JSON.stringify(plug.getDefinition())}</code>}</div>
+      <h2>Channel Receiver</h2>
+      <div>
+        {channel && <code>{JSON.stringify(channel.getDefinition())}</code>}
+      </div>
       <div>
         Last message: <code>{lastMessage}</code>
       </div>
